36f756b29cd1aeafc4c5ff32bcbc7ed15804035e
laufeymatth <laufeymatthildur@gmail.com>
2021-04-14 17:57:09 +0000

    :)

--- tetris_breytt_mhljóði.js
+++ tetris_breytt_mhljóði.js
@@ -259,13 +259,13 @@ function playerDrop() {
 
 	 const colors = [
 	 	null,
-	 	'red',
-	 	'blue',
-	 	'violet',
-	 	'green',
-	 	'purple',
-	 	'orange',
-	 	'pink',
+	 	'#f94144',
+	 	'#f3722c',
+	 	'#f8961e',
+	 	'#f9c74f',
+	 	'#90be6d',
+	 	'#43aa8b',
+	 	'#577590',
 
 	 ];
 
